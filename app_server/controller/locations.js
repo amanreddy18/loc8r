43367a2a989bb423f0d5c2d736d82b85c3739ae2
amanreddy18 <21eg106a58@anurag.edu.in>
2023-08-10 11:22:03 +0530

    final

--- app_server/controller/locations.js
+++ app_server/controller/locations.js
@@ -1,9 +1,22 @@
+/* GET 'home' page */
 module.exports.homelist = function(req, res){
-    res.render('locations-list',{title: 'Home'});
+    res.render('locations-list', { title: 'Home'});
 };
+
+/* GET 'Location info */
 module.exports.locationInfo = function(req, res){
-    res.render('location-info',{title:'Location info'});
+    res.render('location-info', { title: 'Location info'});
 };
+
+module.exports.locationInfo2 = function(req, res){
+    res.render('location-info2', { title: 'Location info'});
+};
+
+module.exports.locationInfo3 = function(req, res){
+    res.render('location-info3', { title: 'Location info'});
+};
+
+/* GET 'ADD review' page */
 module.exports.addReview = function(req, res){
-    res.render('location-review-form',{title: 'Add review'});
+    res.render('location-review-form', { title: 'Add Review'});
 };
--- app_server/controller/others.js
+++ app_server/controller/others.js
@@ -1,3 +1,15 @@
-module.exports.about = function(req, res){
-    res.render('generic-text',{title: 'about'});
+/* GET 'about us'  page */
+module.exports.about = function(req, res)
+{
+    res.render('generic-text', { title: 'About'});
 };
+/* GET 'signin' page */
+module.exports.signin = function(req, res){
+    res.render('signin', { title: 'signin' });
+    content: 'Loc8r is the application you’ll build throughout this book. It displays differently on different devices, showing a list of places and details about each place, and allows visitors to log in and leave reviews.'
+}
+/* GET 'register' page */
+module.exports.register = function(req, res){
+    res.render('register', { title: 'register' });
+    content: 'Loc8r is the application you’ll build throughout this book. It displays differently on different devices, showing a list of places and details about each place, and allows visitors to log in and leave reviews.'
+}
